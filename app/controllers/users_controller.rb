f03a86a24d98453c5b288cb18c50d6ebb571bcab
k.hagio <ec2-user@ip-172-31-6-137.ap-northeast-1.compute.internal>
2022-03-11 13:47:20 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,12 +49,22 @@ class UsersController < ApplicationController
   end
 
   def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
   end
   
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :department, :password_confirmation)
+    end
+    
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
     end
     
     # beforeフィルター
